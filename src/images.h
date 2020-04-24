2081d9953b79b26a1dff4ed0addfd4669584e2ba
Marcus Sanatan <msanatan@gmail.com>
2020-04-23 21:50:54 -0400

    Add the first floor tile
    Depending on how this goes we'll do a full tileset later

---
+++ src/images.h
@@ -0,0 +1,13 @@
+#ifndef IMAGES_H
+#define IMAGES_H
+
+#define Image const unsigned char PROGMEM
+
+Image floorTile[] =
+{
+// width, height,
+8, 8,
+0xff, 0xc3, 0xc3, 0xc3, 0xc3, 0xc3, 0xc3, 0xff,
+};
+
+#endif
