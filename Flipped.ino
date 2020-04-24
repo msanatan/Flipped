30ca3e8e97dd167dd313f2d55ec90a8ef7e2a125
Marcus Sanatan <msanatan@gmail.com>
2020-04-23 21:53:05 -0400

    Draw the world on the screen!
    Made a simple function to draw a level and we're in business

--- Flipped.ino
+++ Flipped.ino
@@ -3,6 +3,8 @@
 // Flipped
 
 #include <Arduboy2.h>
+#include "src/images.h"
+#include "src/levels.h"
 Arduboy2 arduboy;
 
 const int FPS = 60;
@@ -15,6 +17,25 @@ void setup()
     arduboy.clear();
 }
 
+void drawWorld(int level[][16], int width, int height, unsigned char PROGMEM tile[], int tileSize)
+{
+    int positionY = 0;
+    for (int y = 0; y < height; y++)
+    {
+        int positionX = 0;
+        for (int x = 0; x < width; x++)
+        {
+            if (level[y][x] > 0)
+            {
+                Sprites::drawOverwrite(positionX, positionY, tile, 0);
+            }
+            positionX = positionX + tileSize;
+        }
+
+        positionY = positionY + tileSize;
+    }
+}
+
 void loop()
 {
     if (!arduboy.nextFrame())
@@ -22,6 +43,8 @@ void loop()
         return;
     }
 
+    // Draw tiles
+    drawWorld(level1, 16, 8, floorTile, 8);
     // Render the state of the game scene
     arduboy.display();
 }
