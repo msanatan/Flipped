e7658dd96e8355b00da473560c264d34da115b6f
Marcus Sanatan <msanatan@gmail.com>
2020-04-26 14:19:08 -0400

    First attempt at collision
    In this one I decided to see if the player was in a platform tile or not.
    However, there are some strange bugs when it comes to left + right.
    Right now, if the player is touching a tile on the ground it counts as left + right as well.
    
    We do need to get the player's position on the map.
    However, we also need to check the rectangles of the platforms with the player.

--- Flipped.ino
+++ Flipped.ino
@@ -10,6 +10,7 @@ Arduboy2 arduboy;
 
 // Game settings
 const int FPS = 60;
+const int TILE_SIZE = 8;
 int gravity = 1;
 unsigned long playerJumpDuration = 0;
 
@@ -42,14 +43,72 @@ void drawWorld(int level[][16], int width, int height, const unsigned char PROGM
     }
 }
 
-void movePlayer()
+bool isValidTile(int x, int y)
 {
-    // Apply gravity
-    if (!player.isJumping())
+    return level1[y][x];
+}
+
+void checkCollision(int worldEndX, int worldEndY)
+{
+    int leftXTileLocation = player.x / TILE_SIZE;                        // 120 - 15
+    int rightXTileLocation = (player.x + player.getSize()) / TILE_SIZE;  // 128 - 16
+    int topYTileLocation = player.y / TILE_SIZE;                         // 40 - 5
+    int bottomYTileLocation = (player.y + player.getSize()) / TILE_SIZE; // 48 - 6
+
+    // Check if player is touching ground
+    if (bottomYTileLocation < worldEndY &&
+        (isValidTile(leftXTileLocation, bottomYTileLocation) ||
+         isValidTile(rightXTileLocation, bottomYTileLocation)))
+    {
+        player.setOnFloor(true);
+    }
+    else
+    {
+        player.setOnFloor(false);
+    }
+
+    // Check if player is touching the top
+    if (isValidTile(leftXTileLocation, topYTileLocation) ||
+        isValidTile(rightXTileLocation, topYTileLocation))
+    {
+        player.setTouchingTop(true);
+    }
+    else
+    {
+        player.setTouchingTop(false);
+    }
+
+    // Check if player is touching on its right
+    if (rightXTileLocation < worldEndX &&
+        (isValidTile(rightXTileLocation, topYTileLocation) ||
+        isValidTile(rightXTileLocation, bottomYTileLocation)))
+    {
+        player.setTouchingRight(true);
+    }
+    else
+    {
+        player.setTouchingRight(false);
+    }
+
+    // Check if player is touching on its left
+    if (isValidTile(leftXTileLocation, topYTileLocation) ||
+        isValidTile(leftXTileLocation, bottomYTileLocation))
+    {
+        player.setTouchingLeft(true);
+    }
+    else
+    {
+        player.setTouchingLeft(false);
+    }
+}
+
+void applyGravity()
+{
+    if (!player.isJumping() && !player.isOnFloor())
     {
         player.y = player.y + gravity;
     }
-    else // Keep going up if jumping
+    else if (player.isJumping()) // Keep going up if jumping
     {
         player.y = player.y - player.speedY;
 
@@ -58,23 +117,26 @@ void movePlayer()
         unsigned long deltaTime = currentTime - playerJumpDuration;
         float deltaSeconds = deltaTime * (1.0f / 1000.0f);
 
-        if (deltaSeconds >= player.getJumpDuration())
+        if ((deltaSeconds >= player.getJumpDuration()) || player.isTouchingTop())
         {
             player.toggleJumping();
             playerJumpDuration = 0;
         }
     }
+}
 
-    if (arduboy.pressed(LEFT_BUTTON) && player.x > 0)
+void movePlayer()
+{
+    if (arduboy.pressed(LEFT_BUTTON) && !player.isTouchingLeft())
     {
         player.x = player.x - player.speedX;
     }
-    else if (arduboy.pressed(RIGHT_BUTTON) && player.x + player.getSize() < WIDTH)
+    else if (arduboy.pressed(RIGHT_BUTTON) && !player.isTouchingRight())
     {
         player.x = player.x + player.speedX;
     }
 
-    if (arduboy.justPressed(B_BUTTON))
+    if (arduboy.justPressed(B_BUTTON) && player.isOnFloor())
     {
         player.y = player.y - player.speedY;
         player.toggleJumping();
@@ -98,11 +160,14 @@ void loop()
     arduboy.clear();
     arduboy.pollButtons();
 
+    // Check collision and gravity
+    checkCollision(16, 8);
+    applyGravity();
     // Move player
     movePlayer();
 
     // Draw tiles
-    drawWorld(level1, 16, 8, floorTile, 8);
+    drawWorld(level1, 16, 8, floorTile, TILE_SIZE);
     // Draw player
     drawPlayer();
 
