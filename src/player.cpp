49a8254ac0a8ea24a8093795103702261ae603f9
Marcus Sanatan <msanatan@gmail.com>
2020-04-25 23:12:15 -0400

    Add getters and setters for Player class
    Captures most data we need to be fully functional

--- src/player.cpp
+++ src/player.cpp
@@ -1,6 +1,6 @@
 #include "player.h"
 
-Player::Player(uint8_t x, uint8_t y, uint8_t speedX, uint8_t speedY,
+Player::Player(uint8_t x, uint8_t y, int speedX, int speedY,
                uint8_t size, float jumpDuration, const unsigned char *image)
 {
     this->x = x;
@@ -10,9 +10,68 @@ Player::Player(uint8_t x, uint8_t y, uint8_t speedX, uint8_t speedY,
     this->size = size;
     this->jumpDuration = jumpDuration;
     this->image = image;
+    this->jumping = false;
+    this->onFloor = false;
+    this->frame = 0;
 }
 
-Player::getRect()
+uint8_t Player::getSize()
 {
-    return Rect { this->x, this->y, this->size, this->size }
+    return this->size;
+}
+
+float Player::getJumpDuration()
+{
+    return this->jumpDuration;
+}
+
+Rect Player::getRect()
+{
+    return Rect{this->x, this->y, this->size, this->size};
+}
+
+bool Player::isJumping()
+{
+    return this->jumping;
+}
+
+void Player::toggleJumping()
+{
+    this->jumping = !this->jumping;
+}
+
+bool Player::isOnFloor()
+{
+    return this->onFloor;
+}
+
+void Player::toggleOnFloor()
+{
+    this->onFloor = !this->onFloor;
+}
+
+uint8_t Player::getFrame()
+{
+    return this->frame;
+}
+
+void Player::setFrame(uint8_t frame)
+{
+    this->frame = frame;
+}
+
+const unsigned char *Player::getImage()
+{
+    return this->image;
+}
+
+void Player::reset(uint8_t x, uint8_t y, int speedX, int speedY)
+{
+    this->x = x;
+    this->y = y;
+    this->speedX = speedX;
+    this->speedY = speedY;
+    this->jumping = false;
+    this->onFloor = false;
+    this->frame = 0;
 }
--- src/player.h
+++ src/player.h
@@ -1,23 +1,37 @@
 #ifndef PLAYER_H
 #define PLAYER_H
 
+#include <Arduboy2.h>
+
 class Player
 {
 private:
-    uint8_t x;
-    uint8_t y;
-    uint8_t speedX;
-    uint8_t speedY;
     uint8_t size;
     float jumpDuration;
     bool jumping;
+    bool onFloor;
     uint8_t frame;
     const unsigned char *image;
 
 public:
-    Player(uint8_t x, uint8_t y, uint8_t speedX, uint8_t speedY, uint8_t size,
+    uint8_t x;
+    uint8_t y;
+    int speedX;
+    int speedY;
+    Player(uint8_t x, uint8_t y, int speedX, int speedY, uint8_t size,
            float jumpDuration, const unsigned char *image);
+    uint8_t getSize();
+    float getJumpDuration();
     Rect getRect();
+    bool isJumping();
+    void toggleJumping();
+    bool isOnFloor();
+    void toggleOnFloor();
+    uint8_t getFrame();
+    void setFrame(uint8_t frame);
+    const unsigned char *getImage();
+    void reset(uint8_t x, uint8_t y, int speedX, int speedY);
+    void draw();
 };
 
 #endif // !PLAYER_H
