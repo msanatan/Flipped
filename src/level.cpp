f0fe42644a47e41c2133fa11f4a88f539f73e019
Marcus Sanatan <msanatan@gmail.com>
2020-05-04 18:53:11 -0400

    The level class now places the end goal

--- src/level.cpp
+++ src/level.cpp
@@ -1,15 +1,22 @@
 #include "level.h"
 
 Level::Level(uint8_t (*map)[16], uint8_t width, uint8_t height, uint8_t startX,
-             uint8_t startY) {
+             uint8_t startY, uint8_t goalX, uint8_t goalY,
+             const unsigned char *goalImage) {
   this->map = map;
   this->width = width;
   this->height = height;
   this->startPosition = Point{startX * 8, startY * 8};
+  this->goalPosition = Point{goalX * 8, goalY * 8};
+  this->goalImage = goalImage;
 }
 
 Point Level::getStartingPoint() { return this->startPosition; }
 
+Point Level::getGoalPosition() { return this->goalPosition; }
+
 uint8_t Level::getWidth() { return this->width; }
 
 uint8_t Level::getHeight() { return this->height; }
+
+const unsigned char *Level::getGoalImage() { return this->goalImage; }
--- src/level.h
+++ src/level.h
@@ -8,14 +8,19 @@ class Level {
   uint8_t width;
   uint8_t height;
   Point startPosition;
+  Point goalPosition;
+  const unsigned char *goalImage;
 
  public:
   uint8_t (*map)[16];
   Level(uint8_t (*map)[16], uint8_t width, uint8_t height, uint8_t startX,
-        uint8_t startY);
+        uint8_t startY, uint8_t goalX, uint8_t goalY,
+        const unsigned char *goalImage);
   Point getStartingPoint();
+  Point getGoalPosition();
   uint8_t getWidth();
   uint8_t getHeight();
+  const unsigned char *getGoalImage();
 };
 
 #endif  // !LEVEL_H
